4368e18d445b0f169ac6fa1d08e7e9dd37c406d1
guidev <guilhermefontes667@gmail.com>
2024-07-23 17:30:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Configuração de CORS
 const corsOptions = {
-  origin: 'https://oficial-dvgv.onrender.com',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'userid'],
   credentials: true,
