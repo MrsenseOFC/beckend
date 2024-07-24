05cf614c2f47c98efe8cfb3aef9eca0992699fbd
guidev <guilhermefontes667@gmail.com>
2024-07-24 19:36:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 7101;
+const PORT = process.env.PORT || 7121;
 app.listen(PORT, () => {
   console.log(`Servidor está rodando na porta ${PORT}`);
 });
