9953419e4bcf3ace8ea7e9e759d0c35f3c6c4d42
guidev <guilhermefontes667@gmail.com>
2024-07-24 14:41:47 -0300

    Descrição das mudanças

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 // Carregar variáveis de ambiente do arquivo .env
@@ -7,6 +7,7 @@ dotenv.config();
 // Crie a conexão com o banco de dados
 export const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Adicione a porta aqui
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
