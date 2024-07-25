eb9c113edd9da198e3cab4217f586628432671ee
guidev <guilhermefontes667@gmail.com>
2024-07-25 00:40:12 -0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,20 @@
-import { db } from '../connect.js';
+import promisePool from '../connect.js';
 
-export const getUser = (req, res) => {
+export const getUser = async (req, res) => {
   const userId = req.user.id;
 
   const query = 'SELECT username FROM users WHERE id = ?';
-  db.query(query, [userId], (err, result) => {
-    if (err) {
-      console.error('Erro ao buscar usuário:', err);
-      return res.status(500).json({ error: 'Erro ao buscar usuário' });
-    }
 
-    if (result.length === 0) {
+  try {
+    const [rows] = await promisePool.query(query, [userId]);
+
+    if (rows.length === 0) {
       return res.status(404).json({ error: 'Usuário não encontrado' });
     }
 
-    res.status(200).json({ username: result[0].username });
-  });
+    res.status(200).json({ username: rows[0].username });
+  } catch (err) {
+    console.error('Erro ao buscar usuário:', err);
+    res.status(500).json({ error: 'Erro ao buscar usuário' });
+  }
 };
