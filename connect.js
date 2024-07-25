a0d8516bdc5aee9e6dae6f39928307fae9f13a6a
guidev <guilhermefontes667@gmail.com>
2024-07-25 00:35:28 -0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -4,44 +4,32 @@ import dotenv from 'dotenv';
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
 
-// Crie a conexão com o banco de dados
-export const db = mysql.createConnection({
+// Crie o pool de conexões com o banco de dados
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, // Adicione a porta aqui
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10, // Número máximo de conexões simultâneas
+    queueLimit: 0 // Número máximo de conexões na fila
 });
 
-// Conectar ao banco de dados
-db.connect((err) => {
-    if (err) {
+// Promisify a pool para usar com async/await
+const promisePool = pool.promise();
+
+// Testar a conexão ao banco de dados
+async function testConnection() {
+    try {
+        const [rows, fields] = await promisePool.query('SELECT 1 + 1 AS solution');
+        console.log('Conexão bem sucedida ao banco de dados MySQL. Resultado da consulta: ', rows[0].solution);
+    } catch (err) {
         console.error('Erro ao conectar ao banco de dados:', err);
-        return;
     }
-    console.log('Conexão bem sucedida ao banco de dados MySQL');
-});
+}
 
-// Lidar com erros de conexão
-db.on('error', (err) => {
-    console.error('Erro na conexão com o banco de dados:', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.error('Conexão com o banco de dados foi fechada');
-    } else {
-        throw err;
-    }
-});
+testConnection();
 
-// Fechar a conexão com o banco de dados ao sair do aplicativo (opcional)
-process.on('SIGINT', () => {
-    db.end((err) => {
-        if (err) {
-            console.error('Erro ao fechar a conexão com o banco de dados:', err);
-        }
-        console.log('Conexão com o banco de dados fechada');
-        process.exit();
-    });
-});
-
-// Exportar a conexão para ser utilizada em outros módulos
-export default db;
+// Exportar o pool de conexões para ser utilizado em outros módulos
+export default promisePool;
