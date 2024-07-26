488ea14d4d8e497a9ac3a3934db302bab1aa5853
guidev <guilhermefontes667@gmail.com>
2024-07-25 23:14:25 -0300

    Update userPhotosController.js

--- controllers/userPhotosController.js
+++ controllers/userPhotosController.js
@@ -1,8 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs'; // Importa o módulo fs
-import pool from '../connect.js'; // Atualize para importar a exportação padrão
-import { v4 as uuidv4 } from 'uuid'; // Importa o UUID para criar identificadores únicos
+import fs from 'fs';
+import pool from '../connect.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,15 +12,12 @@ export const uploadProfilePicture = async (req, res) => {
     return res.status(400).send('No file uploaded.');
   }
 
-  // Cria um nome único para o arquivo
   const uniqueFilename = `${uuidv4()}-${Date.now()}-${req.file.originalname}`;
   const filePath = path.join(__dirname, '..', 'uploads', 'photos', uniqueFilename);
 
   try {
-    // Salva o arquivo no diretório com o novo nome
     await fs.promises.rename(req.file.path, filePath);
 
-    // Atualiza ou insere o caminho do arquivo no banco de dados
     const query = `
       INSERT INTO user_photos (user_id, filePath) 
       VALUES (?, ?) 
@@ -39,7 +36,6 @@ export const getProfilePicture = async (req, res) => {
   const { userId } = req.params;
 
   try {
-    // Consulta o caminho do arquivo no banco de dados
     const query = 'SELECT filePath FROM user_photos WHERE user_id = ?';
     const [result] = await pool.query(query, [userId]);
     const filePath = result[0]?.filePath;
