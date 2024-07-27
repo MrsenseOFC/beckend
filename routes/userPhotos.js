bd2ca0142cdd0cebfa1ef3dcd7278b7d58f184c9
guidev <guilhermefontes667@gmail.com>
2024-07-26 21:07:45 -0300

    Update userPhotos.js

--- routes/userPhotos.js
+++ routes/userPhotos.js
@@ -1,80 +1,12 @@
 import express from 'express';
-import multer from 'multer';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
 import { uploadProfilePicture, getProfilePicture } from '../controllers/userPhotosController.js';
 
 const router = express.Router();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Rota para fazer upload da foto de perfil
+router.post('/upload', uploadProfilePicture);
 
-// Criar o diretório de uploads se não existir
-const uploadsDir = path.join(__dirname, '..', 'uploads', 'photos');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadsDir);
-  },
-  filename: (req, file, cb) => {
-    const uniqueFilename = `${uuidv4()}-${Date.now()}-${file.originalname}`;
-    cb(null, uniqueFilename);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'), false);
-  }
-};
-
-const upload = multer({ 
-  storage,
-  fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 } // 5 MB limit
-});
-
-router.post('/upload', upload.single('profilePicture'), async (req, res, next) => {
-  try {
-    if (!req.file) {
-      return res.status(400).send('No file uploaded.');
-    }
-
-    if (!req.body.user_id) {
-      return res.status(400).send('User ID is required.');
-    }
-
-    // Pass the file and user_id to the controller function
-    await uploadProfilePicture(req.file, req.body.user_id);
-    res.status(200).send('Upload successful.');
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/:userId', async (req, res, next) => {
-  try {
-    const filePath = await getProfilePicture(req.params.userId);
-    res.sendFile(path.join(uploadsDir, filePath));
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.use((err, req, res, next) => {
-  if (err instanceof multer.MulterError) {
-    return res.status(400).send(`Multer error: ${err.message}`);
-  } else if (err) {
-    return res.status(500).send(`Server error: ${err.message}`);
-  }
-});
+// Rota para obter a foto de perfil de um usuário específico
+router.get('/:userId', getProfilePicture);
 
 export default router;
