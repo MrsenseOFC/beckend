24f6d6936f0070fb45f36c645776d4c913ecdc30
guidev <guilhermefontes667@gmail.com>
2024-07-29 07:35:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(helmet({
       scriptSrc: ["'self'", "'unsafe-eval'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https://example.com"],
-      connectSrc: ["'self'", "http://localhost:7320"],
+      connectSrc: ["'self'", "https://oficial-dvgv.onrender.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       objectSrc: ["'none'"],
       frameSrc: ["'none'"],
