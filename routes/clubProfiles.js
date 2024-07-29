d4a0c6539c50c78020d090b23f4b4967ea4cc194
guidev <guilhermefontes667@gmail.com>
2024-07-29 11:30:46 -0300

    Update clubProfiles.js

--- routes/clubProfiles.js
+++ routes/clubProfiles.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getClubProfile } from '../controllers/clubProfilesController.js';
+import { getClubProfile } from '../controllers/clubProfilesController.js'; // Verifique o caminho
 
 const router = express.Router();
 
+// Rota para obter o perfil do clube
 router.get('/:id', getClubProfile);
 
 export default router;
