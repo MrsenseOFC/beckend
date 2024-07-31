7058018441f387b85f665060ddce92fa1b875e01
guidev <guilhermefontes667@gmail.com>
2024-07-30 21:33:06 -0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 import promisePool from '../connect.js';
 
 export const getUser = async (req, res) => {
-  // Check if user ID is present in the request object
   if (!req.user || !req.user.id) {
     return res.status(400).json({ error: 'User ID is required' });
   }
@@ -12,12 +11,10 @@ export const getUser = async (req, res) => {
   try {
     const [rows] = await promisePool.query(query, [userId]);
 
-    // Check if the user was found
     if (rows.length === 0) {
       return res.status(404).json({ error: 'Usuário não encontrado' });
     }
 
-    // Return the username
     res.status(200).json({ username: rows[0].username });
   } catch (err) {
     console.error('Erro ao buscar usuário:', err);
