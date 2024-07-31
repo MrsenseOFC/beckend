e9f6690fb2606b0f7e6380ebe4e098aaf33d07a3
guidev <guilhermefontes667@gmail.com>
2024-07-30 21:50:32 -0300

    Update userPhotos.js

--- routes/userPhotos.js
+++ routes/userPhotos.js
@@ -1,41 +1,21 @@
 import express from 'express';
 import { uploadProfilePicture, getProfilePicture } from '../controllers/userPhotosController.js';
+import { authenticateToken } from '../middlewares/auth.js';
 import multer from 'multer';
-import path from 'path';
-
-// Defina o armazenamento e a lógica de renomeação do arquivo
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + ext);
-  },
-});
-
-// Defina o filtro de arquivo para aceitar apenas imagens
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true);
-  } else {
-    cb(new Error('Não é um tipo de arquivo suportado'), false);
-  }
-};
-
-const upload = multer({ 
-  storage: storage,
-  fileFilter: fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 } // Limite de 5MB
-});
 
 const router = express.Router();
 
-// Rota para fazer o upload da foto de perfil
-router.post('/:userId/upload', upload.single('image_file'), uploadProfilePicture);
+// Setup multer for file uploads
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
 
-// Rota para obter a foto de perfil do usuário
-router.get('/:userId', getProfilePicture);
+const upload = multer({ storage: storage });
+
+router.post('/:userId/upload', authenticateToken, upload.single('image_file'), uploadProfilePicture);
+router.get('/:userId', authenticateToken, getProfilePicture);
 
 export default router;
