d14222a0837c1e2c6be42d912fa818f56f563d8f
guidev <guilhermefontes667@gmail.com>
2024-08-04 22:15:25 -0300

    Update playerProfiles.js

--- routes/playerProfiles.js
+++ routes/playerProfiles.js
@@ -1,11 +1,11 @@
-// src/routes/playerProfilesRoutes.js
+// src/routes/playerProfiles.js
 
 import express from 'express';
-import { getPlayerProfile } from '../controllers/playerProfilesController.js'; // Certifique-se de que o caminho está correto
+import { getPlayerProfile } from '../controllers/playerProfilesController.js'; // Verifique o caminho e o nome da função exportada
 
 const router = express.Router();
 
-// Rota para obter o perfil de um jogador específico pelo ID do usuário
+// Endpoint para obter o perfil de um jogador específico pelo ID do usuário
 router.get('/:userId', getPlayerProfile);
 
 export default router;
